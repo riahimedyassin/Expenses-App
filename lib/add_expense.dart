060578ea7eda5f2550d72b4d6c8dd940fa78ee9a
riahimedyassin <riahimohamedyassin@charguia.r-iset.tn>
2024-01-13 16:09:10 +0100

    Date picker

--- lib/add_expense.dart
+++ lib/add_expense.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; 
 
 class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
@@ -11,6 +12,15 @@ class AddExpense extends StatefulWidget {
 
 class _AddExpenseState extends State<AddExpense> {
   final _titleController = TextEditingController();
+  final _priceController = TextEditingController();
+  late final DateTime? _date;
+  String dateString = 'Select a date'; 
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+    _priceController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +35,64 @@ class _AddExpenseState extends State<AddExpense> {
                 controller: _titleController,
                 maxLength: 50,
                 decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.add),
                     label: Text("Enter the expense name")),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _priceController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.price_change_outlined),
+                          label: Text("Price")),
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(dateString),
+                        IconButton(
+                          onPressed: () async {
+                            final firstDate = DateTime(DateTime.now().year - 1);
+                            final lastDate = DateTime(DateTime.now().year + 1);
+                            final DateTime? selectedDate =
+                                await showDatePicker(
+                              context: context,
+                              firstDate: firstDate,
+                              lastDate: lastDate,
+                            );
+                            setState(() {
+                              if(selectedDate!=null) {
+                                _date = selectedDate;
+                                dateString = DateFormat.yMMMMd().format(selectedDate).toString(); 
+                              }
+                            });
+                          },
+                          icon: const Icon(Icons.date_range),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
               Row(
                 children: [
                   ElevatedButton(
                     onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Cancel",
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
                       log(_titleController.text);
+                      log(_priceController.text);
                     },
                     child: const Text(
                       'Display value',
@@ -42,11 +103,5 @@ class _AddExpenseState extends State<AddExpense> {
             ],
           ),
         ));
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _titleController.dispose();
   }
 }
